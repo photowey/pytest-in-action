d7da66063f4c68d2b6bc8c8ec58c562f9defb0fb
photowey <photowey@gmail.com>
2022-03-30 23:08:58 +0800

    feat #:1.update encrypt_handler.py

--- src/main/python/encrypt/encrypt_handler.py
+++ src/main/python/encrypt/encrypt_handler.py
@@ -14,27 +14,22 @@ import jpype
 
 
 class EncryptHandler:
-    instnace = None
 
     def __init__(self):
-        global instnace
         jar_path = os.path.abspath(os.path.join(os.getcwd(), '../../resources/encrypt/rsa-encryptor.jar'))
         # jdk version
         java_home = os.environ['JAVA_HOME']
         jpype.startJVM('{}/bin/server/jvm.dll'.format(java_home),
                        '-Djava.class.path={}'.format(jar_path), convertStrings=False)
         JClass = jpype.JClass('cn.org.opencharity.encrypt.Encryptor')
-        instnace = JClass()
+        self.instance = JClass()
 
     def encrypt(self, source):
-        global instnace
-
-        result = str(instnace.encrypt(source))
+        result = str(self.instance.encrypt(source))
         return result
 
     def decrypt(self, source):
-        global instnace
-        result = str(instnace.decrypt(source))
+        result = str(self.instance.decrypt(source))
         return result
 
     def shutdown(self):
@@ -43,8 +38,8 @@ class EncryptHandler:
 
 if __name__ == "__main__":
     handler = EncryptHandler()
-    encryptResult = handler.encrypt('123456')
+    encrypt_result = handler.encrypt('123456')
     # RQDOkImMU2vg2m9EJbaDgDzmzofpcqD2HTWl0Awlcn62czLgmJYlkPeX+v2JYn3lsPb3j5S0wAh/HSzUy3ltgtsrZtwYdvU+Mnw0XahqCZ+iXyRdWUGnT45bR3XzKxtg+vGfq2m/kLBsYt0DSZ9K5/k0owMPLauRNJFeL8xQIcwjLK/sTCtti8TFskooJ2n5i7B/KXBddzyOKTPd09lDbzErAgNuCPuo6z+R2c/FYnmnSJ6MlXrGfTP5I9c9snb8v5yttw3duhSSZGBe9zSDsvgWLEjtLlgPpZN112+2K8STOyxlg0pPKY4tv2670NmgER6hCxYfP7FGFmiUP6aSEg==
-    decryptResult = handler.decrypt(encryptResult)
-    assert decryptResult == '123456'
+    decrypt_result = handler.decrypt(encrypt_result)
+    assert decrypt_result == '123456'
     handler.shutdown()
